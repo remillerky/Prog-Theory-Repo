a960505d71b2edfce0a01d1da0d3df8fe4738c92
remillerky <robert.miller168@gmail.com>
2024-12-15 12:56:36 -0500

    Update Racer.cs
    specified parent class

--- Prog Theory/Assets/Scripts/Racer.cs
+++ Prog Theory/Assets/Scripts/Racer.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Racer : MonoBehaviour
 {
+    public string name; // animal & racer names
+    public string size; // S, M, or L
+    public string speedRange; // F, FF, or FFF
+    public float speedValue; // 1 through 5 x speedRange length
+    
     // Start is called before the first frame update
     void Start()
     {
